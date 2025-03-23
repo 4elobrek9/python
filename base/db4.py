400abe4db81678af495cecdae3c256a9c7405497
brother <hacker@gmail.com>
2025-03-23 21:13:54 +0300

    chr

--- base/db4.py
+++ base/db4.py
@@ -3,7 +3,7 @@ import sqlite3
 connection = sqlite3.connect('zoo.db')
 cursor = connection.cursor()
 
-# cursor.execute("DROP TABLE animals")
+cursor.execute("DROP TABLE animals")
 
 connection.commit()
 connection.close()
