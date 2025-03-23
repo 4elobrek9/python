086189974706078de76ecf0370db9d7060662189
brother <hacker@gmail.com>
2025-03-23 20:49:19 +0300

    ///

--- base/db.py
+++ base/db.py
@@ -3,18 +3,18 @@ import sqlite3
 connection = sqlite3.connect('zoo.db')
 cursor = connection.cursor()
 
-# cursor.execute('''
-# CREATE TABLE IF NOT EXISTS animals (
-#     id INTEGER PRIMARY KEY AUTOINCREMENT,
-#     name TEXT NOT NULL,
-#     species TEXT NOT NULL,
-#     age INTEGER
-# )       
-# ''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS animals (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    species TEXT NOT NULL,
+    age INTEGER
+)       
+''')
 
-# cursor.execute("INSERT INTO animals (name, species, age) VALUES ('Лев', 'Хищник', 5)")
-# cursor.execute("INSERT INTO animals (name, species, age) VALUES ('Слон', 'Млекопитающее', 19)")
-# cursor.execute("INSERT INTO animals (name, species, age) VALUES ('Попугай', 'Птица', 2)")
+cursor.execute("INSERT INTO animals (name, species, age) VALUES ('Лев', 'Хищник', 5)")
+cursor.execute("INSERT INTO animals (name, species, age) VALUES ('Слон', 'Млекопитающее', 19)")
+cursor.execute("INSERT INTO animals (name, species, age) VALUES ('Попугай', 'Птица', 2)")
 
 cursor.execute("SELECT * FROM animals")
 rows = cursor.fetchall()
